21498f0c43065eed1f27604d537adff56384c579
jbaudru <33021761+jbaudru@users.noreply.github.com>
2025-05-06 09:47:33 +0200

    Fix: timebudget experiment

--- src/main.py
+++ src/main.py
@@ -193,7 +193,7 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--mode", choices=["ga", "benchmark", "timebudget", "roadnetwork"], 
                         default="ga", help="Mode to run")
     parser.add_argument("-s", "--size", type=int, default=400, help="Graph size")
-    parser.add_argument("-p", "--population", type=int, default=10, help="Population size")
+    parser.add_argument("-p", "--population", type=int, default=20, help="Population size")
     parser.add_argument("-g", "--generations", type=int, default=1000, help="Number of generations")
     parser.add_argument("-i", "--instances", type=int, default=50, help="Number of benchmark instances")
     parser.add_argument("-f", "--file", type=str, default="MON.json", help="Road network file")
